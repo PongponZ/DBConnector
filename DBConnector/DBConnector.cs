c2311c9a4272d726e0fa9188f90fba9b9e3ef5a1
Pongpon Sinlapa <mr.pongpon.sinlapa.m.m@gmail.com>
2021-06-10 16:05:12 +0700

    init

---
+++ DBConnector/DBConnector.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DBConnector
+{
+    public class DBConnector
+    {
+        private SqlConnectionStringBuilder _builder = new SqlConnectionStringBuilder();
+        private SqlConnection _connection;
+
+        public DBConnector()
+        {
+            _builder.DataSource = ConfigurationManager.AppSettings["Host"];
+            _builder.InitialCatalog = ConfigurationManager.AppSettings["DatabaseName"];
+            _builder.UserID = ConfigurationManager.AppSettings["Username"];
+            _builder.Password = ConfigurationManager.AppSettings["Password"];
+
+            _connection = new SqlConnection(_builder.ConnectionString);
+            _connection.Open();
+
+            if (_connection.State == ConnectionState.Open)
+                Console.WriteLine($"Database Connected.");
+
+            _connection.Close();
+        }
+
+        public DBConnector(string host, string databaseName, string username, string password)
+        {
+            _builder.DataSource = host;
+            _builder.InitialCatalog = databaseName;
+            _builder.UserID = username;
+            _builder.Password = password;
+
+            _connection = new SqlConnection(_builder.ConnectionString);
+            _connection.Open();
+
+            if (_connection.State == ConnectionState.Open)
+                Console.WriteLine($"Database Connected.");
+
+            _connection.Close();
+        }
+
+        public bool Exec(string statement)
+        {
+            return Exec(statement, null);
+        }
+
+        public bool Exec(string statement, Parameters[] parameters)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(statement, _connection);
+
+                if (parameters != null)
+                    cmd.Parameters.Add(ConvertParam(parameters)); ;
+
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+
+                return true;
+            }
+            catch (SqlException err)
+            {
+                Console.WriteLine(err);
+                return false;
+            }
+
+        }
+
+        public DataTable Query(string statement)
+        {
+            return Query(statement, null);
+        }
+
+
+        public DataTable Query(string statement, Parameters[] parameters)
+        {
+            try
+            {
+                _connection.Open();
+
+                SqlCommand cmd = new SqlCommand(statement, _connection);
+
+
+                if (parameters != null)
+                    cmd.Parameters.Add(ConvertParam(parameters)); ;
+
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable table = new DataTable();
+
+                adapter.Fill(table);
+                adapter.Dispose();
+                cmd.Dispose();
+
+                _connection.Close();
+
+
+                return table;
+            }
+            catch (SqlException err)
+            {
+                Console.WriteLine(err);
+                return null;
+            }
+
+        }
+
+        public DataTable StoreQuery(string statement)
+        {
+            return StoreQuery(statement, null);
+        }
+
+        public DataTable StoreQuery(string storeName, Parameters[] parameters)
+        {
+            DataTable table = new DataTable();
+            SqlCommand cmd = _connection.CreateCommand();
+
+            cmd.CommandText = storeName;
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            if (parameters != null)
+                cmd.Parameters.Add(ConvertParam(parameters));
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            adapter.Fill(table);
+            adapter.Dispose();
+            cmd.Dispose();
+
+            return table;
+        }
+
+        public bool ExecStore(string storeName)
+        {
+            return ExecStore(storeName, null);
+        }
+
+        public bool ExecStore(string storeName, Parameters[] parameters)
+        {
+            try
+            {
+                SqlCommand cmd = _connection.CreateCommand();
+
+                cmd.CommandText = storeName;
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                if (parameters != null)
+                    cmd.Parameters.Add(ConvertParam(parameters));
+
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+
+                return true;
+            }
+            catch (SqlException err)
+            {
+                Console.WriteLine(err);
+                return false;
+            }
+        }
+
+        private SqlParameter[] ConvertParam(Parameters[] param)
+        {
+
+            SqlParameter[] s = new SqlParameter[param.Length];
+
+            for (int i = 0; i < param.Length; i++)
+            {
+                s[i] = new SqlParameter(param[i].ParameterName, param[i].Value);
+            }
+
+            return s;
+        }
+
+    }
+
+    public class Parameters
+    {
+        public string ParameterName { get; set; }
+        public object Value { get; set; }
+    }
+}
